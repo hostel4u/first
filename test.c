51e2570f73ceed2b73a784c4ef992db38c364a67
chaatbottestowy <57325439+chaatbottestowy@users.noreply.github.com>
2020-01-21 18:49:25 +0100

    Add files via upload

---
+++ test.c
@@ -0,0 +1,14 @@
+#tsting branch
+# first program with ubuntu
+
+#include <stdio.h>
+
+string get_string(string prompt);
+int printf(const char *format, ...);
+
+int main(void) 
+{
+	string name = get_string("Name: ");
+	printf("hello, %s\n", name);
+	printf("hello world from ubuntu 18.04");
+}
